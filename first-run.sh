2c97f7426fdb459aed8f5c856069c7bbd0f0e560
syswraith <syswraith@proton.me>
2026-02-01 18:19:34 +0530

    added max sound exceed

--- first-run.sh
+++ first-run.sh
@@ -1,2 +1,3 @@
 sudo mv /usr/share/sounds/freedesktop/stereo/camera-shutter.oga /usr/share/sounds/freedesktop/stereo/camera-shutter-disabled.oga
 gsettings set org.gnome.SessionManager logout-prompt false
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
