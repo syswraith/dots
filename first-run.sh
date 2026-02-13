64d42b7690f577122a95ecfaaf9866b580198634
syswraith <syswraith@proton.me>
2026-02-13 20:26:56 +0530

    Friday 13 February 2026 08:26:36 PM IST

--- first-run.sh
+++ first-run.sh
@@ -1,3 +1,4 @@
 sudo mv /usr/share/sounds/freedesktop/stereo/camera-shutter.oga /usr/share/sounds/freedesktop/stereo/camera-shutter-disabled.oga
 gsettings set org.gnome.SessionManager logout-prompt false
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+gsettings set org.gnome.mutter center-new-windows true
